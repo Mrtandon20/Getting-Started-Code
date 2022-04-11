cbb404baf29f492089ace7478e1be87134842fbe
Varun Tandon <75672315+Mrtandon20@users.noreply.github.com>
2022-04-12 00:17:04 +0530

    Sum of the Two Integer Numbers

---
+++ Sum_of_Two_No.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n1;
+    cout<<"Enter the first No : ";
+    cin>>n1;
+    cout<<endl;
+    
+    int n2;
+    cout<<"Enter the Second No : ";
+    cin>>n2;
+    cout<<endl;
+
+    int ans;
+    ans = n1+n2;
+    cout<<"Answer : "<<ans<<endl;
+}
